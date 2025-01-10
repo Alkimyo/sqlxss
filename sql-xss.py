548ef4b9fd29e647265692c4742a7fe8ca5bdc40
Alkimyo <164453140+Alkimyo@users.noreply.github.com>
2025-01-10 19:59:56 +0500

    Update sql-xss.py

--- sql-xss.py
+++ sql-xss.py
@@ -4,11 +4,9 @@ import pickle
 import tensorflow as tf
 import pandas as pd
 
-# Modelni yuklash
 with open('model.pkl', 'rb') as f: 
     model = pickle.load(f)
 
-# Kirish ma'lumotlarini tayyorlash funksiyalari
 def data2char_index(X, max_len, is_remove_comment=False):
     alphabet = " abcdefghijklmnopqrstuvwxyz0123456789-,;.!?:'\"/\\|_@#$%^&*~`+-=<>()[]{}"
     result = []
@@ -40,10 +38,8 @@ def data_to_symbol_tag(X, max_len, is_remove_comment=False):
         result.append(mat)
     return tf.keras.preprocessing.sequence.pad_sequences(np.array(result), padding='post', truncating='post', maxlen=max_len)
 
-# Streamlit ilovasi
 st.set_page_config(page_title="SQL Injection, XSS va Normal Matnni Tahlil Qilish", page_icon=":guardsman:", layout="wide")
 
-# Sahifaning fonini o'zgartirish
 st.markdown(
     """
     <style>
@@ -68,27 +64,21 @@ st.markdown(
     </style>
     """, unsafe_allow_html=True)
 
-# Bosh sahifa dizayni
 st.title('SQL Injection, XSS Detector')
 
-# Matn kiritish
 input_text = st.text_area("Buyruq kiriting:", height=150)
 
-# Modelni ishga tushirish tugmasi
 if st.button('Tahlil qilish'):
     if input_text:
-        # Kiruvchi qiymatlarni tayyorlash
         max_len = 1000
         input_text_encoded = data2char_index([input_text], max_len)
         input_symbol_encoded = data_to_symbol_tag([input_text], max_len)
 
-        # Modelga yuborish
+      
         prediction = model.predict([input_text_encoded, input_symbol_encoded])
 
-        # Chiquvchi natijalarni 10 darajasi -2 gacha yaxlitlash
         prediction_rounded = np.round(prediction, decimals=2)
 
-        # Jadval formatida natijalarni chiqarish
         st.write("Modelning prognozi:")
         result_df = pd.DataFrame({
             'Buyruqlar': ['SQL Injection', 'XSS', 'Normal'],
